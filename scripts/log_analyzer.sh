13ff83a858077a70bc3bf5c5521f40106273c8b1
vladimir <v.g.galkin@gmail.com>
2026-01-21 20:17:22 +0300

    Fix log_analyzer script

--- scripts/log_analyzer.sh
+++ scripts/log_analyzer.sh
@@ -1,12 +1,23 @@
-#!/bin/bash
-
-total_requests=$(wc -l)
-
-status_200=$(awk '$NF == 200 {count++} END {print count+0}')
-
-top_ips=$(awk '{print $1}' | sort | uniq -c | sort -nr | head -3)
-
-echo "Общее количество HTTP-запросов: $total_requests"
-echo "Количество ответов с кодом 200: $status_200"
-echo "ТОП-3 IP-адреса по количеству запросов:"
-echo "$top_ips"
+awk '
+{
+    total++
+    ip[$1]++
+    if ($NF == 200) status200++
+}
+END {
+    print "Общее количество HTTP-запросов:", total
+    print "Количество ответов с кодом 200:", status200
+    print "ТОП-3 IP-адреса по количеству запросов:"
+    for (i in ip) {
+        printf "%d %s\n", ip[i], i
+    }
+}' |
+{
+    read line1
+    read line2
+    read line3
+    echo "$line1"
+    echo "$line2"
+    echo "$line3"
+    sort -nr | head -3
+}
